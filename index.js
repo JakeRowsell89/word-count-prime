95bcc414f51c2bc1e1b1ae702ad7e2ceb7a9b11d
Jake Rowsell <jake@qubit.com>
2017-04-08 22:26:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// read input text
+// strip characters
+// collect words
+// increase counts
+// when all input done get word->counts
+// for each count determine if prime
---
+++ test/read-file.test.js
@@ -0,0 +1,5 @@
+const readFile = require('../lib/read-file')
+
+test('readFile returns a readable stream', () => {
+  expect(1).toEqual(1)
+})
