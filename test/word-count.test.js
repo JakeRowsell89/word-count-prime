806da2cdb77aa6d3a75f261548b4ea37a7cb282a
Jake Rowsell <jake@qubit.com>
2017-04-09 16:42:09 +0100

    Word counting functionality added

---
+++ test/word-count.test.js
@@ -0,0 +1,33 @@
+const stream = require('stream')
+const wordCount = require('../lib/word-count')
+
+const createStreamAndWriteInput = (input) => {
+  const readStream = new stream.PassThrough()
+  readStream.end(input)
+  return readStream
+}
+
+test('wordCount() returns a Promise', () => {
+  expect(wordCount()).toBeInstanceOf(Promise)
+})
+
+test('wordCount() can take a stream with 1 word and return {<word>: 1}', () => {
+  const input = 'thing'
+  const readStream = createStreamAndWriteInput(input)
+
+  return wordCount(readStream).then(data => expect(data).toEqual({ 'thing': 1 }))
+})
+
+test('wordCount() can take a stream with 2 the same words and return {<word>: 2}', () => {
+  const input = 'thing thing'
+  const readStream = createStreamAndWriteInput(input)
+
+  return wordCount(readStream).then(data => expect(data).toEqual({ 'thing': 2 }))
+})
+
+test('wordCount() can take a stream with 2 different words and return {<word1>: 1, <word2>: 1}', () => {
+  const input = 'thing thang'
+  const readStream = createStreamAndWriteInput(input)
+
+  return wordCount(readStream).then(data => expect(data).toEqual({ 'thing': 1, 'thang': 1 }))
+})
