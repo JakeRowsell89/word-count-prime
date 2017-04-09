ef65f58ff1e3e555408cd3da1befb8a75f16c6b1
Jake Rowsell <jake@qubit.com>
2017-04-09 13:13:14 +0100

    Readable stream returns contents as put in file

--- test/read-file.test.js
+++ test/read-file.test.js
@@ -3,7 +3,7 @@ const path = require('path')
 const { Readable } = require('stream')
 const readFile = require('../lib/read-file')
 const existantFile = path.resolve(__dirname, './test-file.txt')
-const existantFileRelative = path.relative('../lib/read-file', existantFile)
+// const existantFileRelative = path.relative(path.resolve(__dirname, '../lib/read-file'), existantFile)
 const fileContents = 'Test'
 
 beforeAll(() => {
@@ -19,25 +19,23 @@ test('readFile returns a promise', () => {
 })
 
 test('Attempting to read an non-existing file will throw an error', () => {
-  return readFile(existantFile).catch(e => {
-    expect(e.message).toEqual('file not found')
-  })
+  return readFile(existantFile).catch(e => expect(e.message).toEqual('file not found'))
 })
 
 test('Reading an existing file will return a promise containing a Readable stream', () => {
-  return readFile(existantFileRelative).then(data => {
-    expect(data).toBeInstanceOf(Readable)
-  })
+  return readFile(existantFile).then(data => expect(data).toBeInstanceOf(Readable))
 })
 
-// test('Readable stream contents after completion should be the same as fileContents', () => {
-//   return readFile(existantFileRelative).then(data => {
-//     let result = ''
-//     data.on('data', (chunk) => {
-//       result += chunk
-//     })
-//     data.on('end', () => {
-//       expect(result).toEqual(fileContents)
-//     })
-//   })
-// })
+test('Readable stream contents after completion should be the same as fileContents', () => {
+  return readFile(existantFile).then(stream => {
+    return new Promise((resolve, reject) => {
+      let result = ''
+      stream.on('data', (chunk) => {
+        result += chunk
+      })
+      stream.on('end', () => {
+        resolve(result === fileContents)
+      })
+    }).then(data => expect(data).toEqual(true))
+  })
+})
