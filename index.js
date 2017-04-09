960811c8fbddceb73ec44b60d753de112372f8c7
Jake Rowsell <jake@qubit.com>
2017-04-09 23:00:58 +0100

    console.table seemed to cause some problems as a bin executable, switched for tabular

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('console.table')
+const tabula = require('tabula')
 const { getWordCountAndIfPrime } = require('./lib/main')
 const file = process.argv.slice(2)[0]
-const title = `Occurrences of words in ${file}`
 
 getWordCountAndIfPrime(file).then(words => {
   process.stdout.write('\u001B[2J\u001B[0;0F')
-  console.table(title, words)
+  process.stdout.write(`Occurrences of words in: ${file}\n\n`)
+  tabula(words)
 }).catch(err => {
   process.stdout.write(err.message + '\n')
 })
